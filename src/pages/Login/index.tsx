e12a720c8d198d8c70ce1912d706b9ff5fc51545
ranieriiuri <iuriranierioliveira@gmail.com>
2025-06-22 23:41:05 -0300

    more visual changes on Auth's page

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -8,22 +8,22 @@ export default function Login() {
   const navigate = useNavigate()
 
   return (
-    <div className="flex flex-col min-h-screen w-full max-w-4xl px-4">
+    <div className="flex flex-col min-h-screen w-full px-0">
       {/* Conteúdo centralizado */}
         <main className="flex-1 flex items-center justify-center py-12">
-            <div className="max-w-5xl w-full flex flex-col md:flex-row items-center justify-between gap-8 px-4">
+            <div className="max-w-4xl w-full flex flex-col md:flex-row items-center justify-between gap-8 px-4">
                 
                 {/* Coluna 1 - texto e logo */}
                 <div className="flex-1 space-y-4 text-center md:text-left">
                 <div className="flex items-center justify-center md:justify-start gap-4">
                     <img src="/eclesial.svg" alt="Logo" className="h-18" />
-                    <h1 className="text-3xl font-bold">Eclesial</h1>
+                    <h1 className="text-7xl font-bold">Eclesial</h1>
                 </div>
                 <p className="text-muted-foreground text-amber-600">
-                    Uma plataforma de gestão de arrecadações...
+                    Uma plataforma de gestão de arrecadações para igrejas...
                 </p>
                 <p className="text-muted-foreground text-amber-600">
-                    Pensada e projetada pra viabilizar que cada gesto de amor em forma de doação faça a diferença!
+                    Pensada e projetada pra que cada gesto de amor em forma de doação faça a diferença!
                 </p>
                 </div>
 
@@ -38,7 +38,7 @@ export default function Login() {
                         <Button type="submit" className="w-full text-white hover:text-amber-600">Entrar</Button>
                     </form>
                     <p className="text-sm text-center mt-4 text-gray-400">
-                        Não tem conta?{" "}
+                        Ainda não tem conta?{" "}
                         <span 
                         className="text-amber-600 hover:text-blue-400 cursor-pointer transition-colors duration-200"
                         onClick={() => navigate("/register")}
@@ -55,16 +55,17 @@ export default function Login() {
 
 
       {/* Rodapé */}
-      <footer className="w-full py-4 text-center bg-zinc-200 text-zinc-800 flex flex-col items-center gap-2">
-        <p>ranieriiuri Developments © 2025</p>
-        <div className="flex gap-4">
+      <footer className="w-full h-30 px-6 bg-zinc-200 text-zinc-800 relative flex items-center justify-between">
+        <p className="text-sm absolute left-6">ranieriiuri Developments © 2025</p>
+        <div className="absolute left-1/2 transform -translate-x-1/2 flex items-center gap-4">
+        <h2>Conheça meus outros projetos:</h2>
           <a
             href="https://www.linkedin.com/in/ranieriiuri/"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-white transition"
           >
-            <FaLinkedin className="text-2xl text-slate-700" />
+            <FaLinkedin className="text-2xl text-slate-700 hover:text-amber-700" />
           </a>
           <a
             href="https://github.com/ranieriiuri"
@@ -72,7 +73,7 @@ export default function Login() {
             rel="noopener noreferrer"
             className="hover:text-white transition"
           >
-            <FaGithub className="text-2xl text-slate-700" />
+            <FaGithub className="text-2xl text-slate-700 hover:text-amber-700" />
           </a>
         </div>
       </footer>
